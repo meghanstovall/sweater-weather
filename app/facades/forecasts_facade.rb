63676bc6c80020beacffdc79da21013a251fe635
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-19 12:30:30 -0600

    Add to facade the weather data

--- app/facades/forecasts_facade.rb
+++ app/facades/forecasts_facade.rb
@@ -1,6 +1,6 @@
 class ForecastsFacade
 
-  attr_reader :id, :location, :latitude, :longitude
+  attr_reader :id, :location, :latitude, :longitude, :current, :hourly, :daily
 
   def initialize(location)
     coords = GoogleService.get_coordinates(location)
@@ -8,5 +8,10 @@ class ForecastsFacade
     @location = coords[:results][0][:formatted_address]
     @latitude = coords[:results][0][:geometry][:location][:lat]
     @longitude = coords[:results][0][:geometry][:location][:lng]
+
+    data = WeatherService.get_forecast(@latitude, @longitude)
+    @current = data[:current]
+    @hourly = data[:hourly]
+    @daily = data[:daily]
   end
 end
