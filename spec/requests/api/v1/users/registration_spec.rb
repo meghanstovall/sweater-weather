39679fd43a938816559dadee07533eafabdac19f
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-20 16:09:13 -0600

    Add more sad path tests

--- spec/requests/api/v1/users/registration_spec.rb
+++ spec/requests/api/v1/users/registration_spec.rb
@@ -1,8 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "User Registration" do
+  before :each do
+    @user = User.create!(email: "meghan.stovall@gmail.com", password: "password1")
+  end
+
   it "can send json with user email, password, and password confirmation" do
-    user = {email: "meghan.stovall@gmail.com", password: "pass1", password_confirmation: "pass1"}
+    user = {email: "meghan@gmail.com", password: "pass1", password_confirmation: "pass1"}
 
     post "/api/v1/users", params: user
 
@@ -16,18 +20,31 @@ RSpec.describe "User Registration" do
     expect(json[:attributes]).to have_key(:api_token)
   end
 
-  it "throuws 400 level error if credentials were bad" do
-    user = User.create!(email: "meghan.stovall@gmail.com", password: "pass1")
-    user2 = {email: "email@gmail.com", password: user.password, password_confirmation: user.password_confirmation}
+  it "throuws 400 level error if email is already in use" do
+    user = {email: @user.email, password: "pass2", password_confirmation: "pass2"}
 
-    post "/api/v1/users", params: user2
+    post "/api/v1/users", params: user
 
     new_user = User.last
 
     json = JSON.parse(response.body, symbolize_names: true)[:data]
-require "pry"; binding.pry
+
     expect(json[:attributes][:status]).to eq(406)
     expect(json[:attributes][:message]).to eq("Not Acceptable")
-    expect(json[:attributes][:errors]).to eq("Password digest can't be blank and Password can't be blank")
+    expect(json[:attributes][:errors]).to eq("Email has already been taken")
+  end
+
+  it "throuws 400 level error if passwords dont match" do
+    user = {email: "email@gmail.com", password: "pass1", password_confirmation: "pass2"}
+
+    post "/api/v1/users", params: user
+
+    new_user = User.last
+
+    json = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(json[:attributes][:status]).to eq(406)
+    expect(json[:attributes][:message]).to eq("Not Acceptable")
+    expect(json[:attributes][:errors]).to eq("Password confirmation doesn't match Password")
   end
 end
