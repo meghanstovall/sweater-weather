e28e50dcdd68229ac11aed3384c619a2ced8b233
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-20 14:46:20 -0600

    renamed the forecast serializer

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::ForecastsController < ApplicationController
 
   def show
     data = ForecastsFacade.new(params[:location])
-    render json: ForecastsSerializer.new(data)
+    render json: ForecastSerializer.new(data)
   end
 end
