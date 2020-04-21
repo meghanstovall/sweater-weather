2457fdcdd2eb40ff7fc52ea0532e3e7e8c42053d
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-20 20:59:47 -0600

    Test the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates_uniqueness_of :email
   validates_presence_of :password_digest
-  validates :api_key, uniqueness: true
+  validates_uniqueness_of :api_key
 
   has_secure_password
   has_secure_token :api_key
