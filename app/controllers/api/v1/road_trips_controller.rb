fe57aad8bbb100ba0da27eb139adce1f5ebd29f0
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-20 21:00:19 -0600

    Add conditional in road trip controller for throwing a 401 error

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::RoadTripsController < ApplicationController
 
   def create
-    render json: RoadTripSerializer.new(RoadTripsFacade.new(params[:origin], params[:destination], params[:api_key]))
+    if params[:api_key].empty? || !User.exists?(api_key: params[:api_key])
+      response.status = :unauthorized
+      render json: ResponseSerializer.new(Response.new(response))
+    else
+      render json: RoadTripSerializer.new(RoadTripsFacade.new(params[:origin], params[:destination], params[:api_key]))
+    end
   end
 end
